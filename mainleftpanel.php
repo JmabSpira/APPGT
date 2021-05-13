7e4a8210a3d426dfc7f46bb6ea2756910bc55737
JmabSpira <julio.ayala.27@unsch.edu.pe>
2021-05-13 00:44:50 -0500

    Codigo resumido

---
+++ mainleftpanel.php
@@ -0,0 +1,26 @@
+    <!-- ########## START: LEFT PANEL ########## -->
+    <div class="br-logo"><a href=""><span>[</span>APP GT<span>]</span></a></div>
+    <div class="br-sideleft overflow-y-auto">
+      <label class="sidebar-label pd-x-15 mg-t-20">Navegación</label>
+      <div class="br-sideleft-menu">
+        <a href="../Home/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Inicio</span>
+          </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+
+        <a href="#" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
+            <span class="menu-item-label">Mantenimiento</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div>
+        </a>
+        <ul class="br-menu-sub nav flex-column">
+          <li class="nav-item"><a href="../MntProducto/" class="nav-link">Producto</a></li>
+          <li class="nav-item"><a href="../MntProducto/" class="nav-link">Producto 2</a></li>
+        </ul>
+      </div>
+    </div><!-- br-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
