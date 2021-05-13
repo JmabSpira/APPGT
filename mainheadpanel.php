5fd28967036442578a76a34b662ef05b5c6afa4b
JmabSpira <julio.ayala.27@unsch.edu.pe>
2021-05-13 01:24:25 -0500

    Inicio Finalizado

--- mainheadpanel.php
+++ mainheadpanel.php
@@ -14,18 +14,18 @@
         <nav class="nav">
           <div class="dropdown">
             <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
-              <span class="logged-name hidden-md-down">Katherine</span>
+              <span class="logged-name hidden-md-down">Grados y Títulos</span>
               <img src="http://via.placeholder.com/64x64" class="wd-32 rounded-circle" alt="">
               <span class="square-10 bg-success"></span>
             </a>
             <div class="dropdown-menu dropdown-menu-header wd-200">
               <ul class="list-unstyled user-profile-nav">
-                <li><a href=""><i class="icon ion-ios-person"></i> Edit Profile</a></li>
-                <li><a href=""><i class="icon ion-ios-gear"></i> Settings</a></li>
-                <li><a href=""><i class="icon ion-ios-download"></i> Downloads</a></li>
-                <li><a href=""><i class="icon ion-ios-star"></i> Favorites</a></li>
-                <li><a href=""><i class="icon ion-ios-folder"></i> Collections</a></li>
-                <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+                <li><a href=""><i class="icon ion-ios-person"></i> Editar Perfil</a></li>
+                <li><a href=""><i class="icon ion-ios-gear"></i> Configuraci{on</a></li>
+                <li><a href=""><i class="icon ion-ios-download"></i> Descargas</a></li>
+                <li><a href=""><i class="icon ion-ios-star"></i> Favoritos</a></li>
+                <li><a href=""><i class="icon ion-ios-folder"></i> Colección</a></li>
+                <li><a href=""><i class="icon ion-power"></i> Salir</a></li>
               </ul>
             </div><!-- dropdown-menu -->
           </div><!-- dropdown -->
---
+++ view/Home/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <?php require_once("../../mainhead.php");?>
+    
+    <title>Mantenimiento de Producto</title>
+  </head>
+
+  <body>
+
+    <?php require_once("../../mainheadpanel.php");?>
+    <?php require_once("../../mainleftpanel.php");?>
+
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="index.html">INICIO</a>
+          <!-- <span class="breadcrumb-item active">Producto</span> -->
+        </nav>
+      </div><!-- br-pageheader -->
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">SISTEMA DE GRADOS Y TÍTULOS</h4>
+        <p class="mg-b-0">UNIVERSIDAD NACIONAL DE SAN CRISTÓBAL DE HUAMANGA</p>
+      </div>
+
+      <div class="br-pagebody">
+        <div class="br-section-wrapper">
+            <h1 class="tx-inverse" align = "center">BIENVENIDO</h1>
+            <div class="row justify-content-center" >
+              <div class="col-md-6">
+                <div class="card">
+                  <img class="card-img-top img-fluid" src="../../public/img/logounsch.png" alt="Image">
+                  <div class="card-body bd bd-t-0 rounded-bottom">
+                    <p class="card-text">SISTEMA DE GRADOS Y TÍTULOS UNSCH</p>
+                  </div>
+              </div><!-- card -->
+              </div>
+            </div>
+        </div>
+      </div>
+      <footer class="br-footer">
+        <div class="footer-left">
+          <div class="mg-b-2">Copyright &copy; 2021. JmabSpira. Todos los derechos reservados.</div>
+          <div>Desarrollador por Julio M. Ayala Bellido - UNSCH.</div>
+        </div>
+      </footer>
+
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+
+
+    <?php require_once("../../mainjs.php");?>
+
+    <!--<script type="text/javascript" src="mntproducto.js"></script>-->
+  </body>
+</html>
